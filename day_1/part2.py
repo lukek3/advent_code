ce8f58df36d44907b45958171ed6fd8c7e782347
Luke Kaczynski <80867156+lukek3@users.noreply.github.com>
2024-12-01 10:37:48 -0500

    Update part2.py

--- day_1/part2.py
+++ day_1/part2.py
@@ -1,21 +1,21 @@
 # --- part 2
 from collections import Counter
 
-# Read data
+# read data
 with open('day_1/input.txt', 'r') as fp:
     input = fp.readlines()
 
-# List of lists for input rows
+# list of lists for input rows
 nums = [x.split() for x in input]
 
-# Parse numbers into separate lists
+# parse numbers into separate lists
 nums_left = [int(left[0]) for left in nums]
 nums_right = [int(right[1]) for right in nums]
 
-# Count occurrences of each number in nums_right, only care about numbers that appear in both lists
+# count occurrences of each number in nums_right, only care about numbers that appear in both lists
 right_counts = Counter(nums_right)
 
-# Calculate the score
+# calculate the score
 score = sum(number * right_counts.get(number, 0) for number in nums_left)
 
 print(score)
